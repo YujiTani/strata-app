74e66407b2e975297bdde62abbae1445d4974645
yuzunosk55 <yuzuha3ds@gmail.com>
2026-07-10 20:04:11 +0900

    fix: Dockerfile とアプリケーションのポートを 8080 に統一し、fly.toml を追加

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia, file } from "elysia";
+import { Elysia } from "elysia";
 
 const items = new Elysia({ prefix: "items" })
 	.get("/", () => "Items List")
@@ -9,20 +9,5 @@ new Elysia()
 	.use(items)
 	.get("/", () => "Hello World")
 	.get("/health", () => "Hi! I'm healthy!")
-	.get("/users/:id", ({ params: { id } }) => `User ID: ${id}`)
-	.get("/users/:id/profile", file("../assets/xIcon.png"))
-	.get("context", (context) => {
-		console.log(context);
-		return context.body;
-	})
-	.get("/status", ({ status }) => status(200, "ok"))
-	.get("set", ({ set }) => {
-		set.status = 201;
-		return "ok bokujou!";
-	})
-	.get("cookie", ({ cookie: name }) => {
-		console.log(name.value);
-		name.value = "とどろきくん";
-	})
 
-	.listen(3001);
+	.listen(8080);
